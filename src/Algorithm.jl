29136859df9995ad1a394b4cf61d015e61df7a24
ShotaTakahashi <shota0418machinery@gmail.com>
2023-12-29 17:14:17 +0900

    fix naming, I/O

--- src/Algorithm.jl
+++ src/Algorithm.jl
@@ -6,11 +6,12 @@ module Algorithm
 
     mutable struct IterativeAlgorithm
         xk::Vector{Float64}
-        xk_1::Vector{Float64}
+        xk_old::Vector{Float64}
         maximum_iter::Int64
         tol::Float64
+        df::DataFrame
 
-        IterativeAlgorithm(x0, maximum_iter, tol) = new(x0, x0, maximum_iter, tol)
+        IterativeAlgorithm(x0, maximum_iter, tol) = new(x0, x0, maximum_iter, tol, DataFrame([Int64[], Float64[]], ["iter", "obj"]))
     end
 
     struct Param
@@ -18,28 +19,14 @@ module Algorithm
         theta::Float64
     end
 
-    function run(alg::IterativeAlgorithm, obj, grad, update, param::Param, result_dir="")
-        is_write = false
-        if result_dir !== ""
-            is_write = true
-            file = open(result_dir, "w+")
-            close(file)
-            df = DataFrame(:iter => [0], :obj => obj(alg.xk))
-        end
+    function run(alg::IterativeAlgorithm, obj, grad, update, param::Param)
+        push!(alg.df, (0, obj(alg.xk)))
         for i = 1:alg.maximum_iter
-            alg.xk_1, alg.xk = alg.xk, update(alg.xk, grad, param)
-            if is_write
-                push!(df, (i, obj(alg.xk)))
-            end
-            if stop_criteria(alg.xk_1, alg.xk, alg.tol)
-                if is_write
-                    CSV.write(result_dir, df)
-                end
+            alg.xk_old, alg.xk = alg.xk, update(alg.xk, grad, param)
+            push!(alg.df, (i, obj(alg.xk)))
+            if stop_criteria(alg.xk_old, alg.xk, alg.tol)
                 return alg.xk
             end
-        end
-        if is_write
-            CSV.write(result_dir, df)
         end
         return alg.xk
     end
@@ -47,11 +34,11 @@ module Algorithm
     function update_ISTA(x::Vector{Float64}, grad, param::Param)
         grad = grad(x)
         v = x - grad .* param.stepsize
-        v = Operator.soft_thresholding(v, param.theta)
+        v = Operator.soft_thresholding(v, param.theta * param.stepsize)
         return v
     end
 
-    run_ISTA(alg, obj, grad, param, result_dir) = run(alg, obj, grad, update_ISTA, param, result_dir)
+    run_ISTA(alg, obj, grad, param) = run(alg, obj, grad, update_ISTA, param)
 
     stop_criteria(x, y, tol) = norm(x - y, 2) < tol
 end
--- src/LeastSquares.jl
+++ src/LeastSquares.jl
@@ -8,8 +8,8 @@ using Plots
 include("Algorithm.jl")
 include("Operator.jl")
 
-loss(x) = norm(A*x - b)^2
-reg(x) = θ*sum(abs.(x))
+loss(x) = norm(A*x - b)^2/2
+reg(x) = theta * sum(abs.(x))
 obj(x) = loss(x) + reg(x)
 
 grad(x) = A' * (A *x - b)
@@ -18,29 +18,30 @@ rng = MersenneTwister(1234)
 n = 200
 m = 300 # 自由に変えてもよいですが，n < m を推奨．
 sparsity = 0.05
+theta = .00015 # θ とすることも可能
 
 A = randn(rng, Float64, (m, n))
 foreach(normalize!, eachcol(A))
 x = Array(sprandn(rng, n, sparsity))
 b = A * x
 
-θ = .00015
-λ = 1.0
+lambda = 1.0
 
 rng = MersenneTwister(5)
 x0 = randn(rng, Float64, n)
 maximum_iter = 10
 tol = 1e-1
 alg = Algorithm.IterativeAlgorithm(x0, maximum_iter, tol)
-param = Algorithm.Param(λ, θ)
+param = Algorithm.Param(lambda, theta)
 
 if !ispath("./results")
     mkdir("./results")
 end
-result_dir = "./results/least-squares_$n-$m-$sparsity-$θ-$λ"
-@time Algorithm.run_ISTA(alg, obj, grad, param, result_dir * ".csv")
+result_dir = "./results/least-squares_$n-$m-$sparsity-$theta-$lambda"
+@time Algorithm.run_ISTA(alg, obj, grad, param)
+CSV.write(result_dir * ".csv", alg.df)
 
-df = CSV.read(result_dir * ".csv", DataFrame)
+df = alg.df
 obj_opt = obj(x)
 plot(df.iter, abs.(df.obj .- obj_opt), xaxis="k", yaxis=raw"$\log_{10}|Ψ(x^k) - Ψ(x^*)|$", label="PG", yscale=:log10)
 savefig(result_dir * "-diffobj.png")
