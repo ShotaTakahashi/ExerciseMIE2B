e3e60422c9b8799c47ac65beacedee3456cba395
ShotaTakahashi <shota0418machinery@gmail.com>
2024-07-08 15:37:20 +0900

    add jupyter notebook for assignment

--- src/Algorithm.jl
+++ src/Algorithm.jl
@@ -11,34 +11,45 @@ module Algorithm
         tol::Float64
         df::DataFrame
 
-        IterativeAlgorithm(x0, maximum_iter, tol) = new(x0, x0, maximum_iter, tol, DataFrame([Int64[], Float64[]], ["iter", "obj"]))
+        IterativeAlgorithm(x0, maximum_iter, tol) = new(x0, x0, maximum_iter, tol, DataFrame([Int64[], Float64[], Float64[]], ["iter", "time", "obj"]))
     end
 
-    struct Param
-        stepsize::Float64
-        theta::Float64
-    end
-
-    function run(alg::IterativeAlgorithm, obj, grad, update, param::Param)
-        push!(alg.df, (0, obj(alg.xk)))
+    function run(alg::IterativeAlgorithm, obj, grad, update, stepsize::Float64, theta::Float64)
+        push!(alg.df, (0, 0.0, obj(alg.xk)))
+        time = 0.0
         for i = 1:alg.maximum_iter
-            alg.xk_old, alg.xk = alg.xk, update(alg.xk, grad, param)
-            push!(alg.df, (i, obj(alg.xk)))
-            if stop_criteria(alg.xk_old, alg.xk, alg.tol)
-                return alg.xk
+            time += @elapsed begin
+                update(alg, grad, stepsize, theta)
+                if stop_criteria(alg.xk_old, alg.xk, alg.tol)
+                    push!(alg.df, (i, time, obj(alg.xk)))
+                    break
+                end
+                push!(alg.df, (i, time, obj(alg.xk)))
             end
         end
         return alg.xk
     end
 
-    function update_ISTA(x::Vector{Float64}, grad, param::Param)
+    function update_ISTA(alg::IterativeAlgorithm, grad, stepsize::Float64, theta::Float64)
+        alg.xk_old, alg.xk = alg.xk, prox(alg.xk, grad, stepsize, theta)
+    end
+
+    # hoge は適切な名称に変えてください．
+    function update_hoge(alg::IterativeAlgorithm, grad, stepsize::Float64, theta::Float64)
+        alg.xk_old, alg.xk = alg.xk, prox(alg.xk, grad, stepsize, theta)
+    end
+
+    function prox(x::Vector{Float64}, grad, stepsize::Float64, theta::Float64)
         grad = grad(x)
-        v = x - grad .* param.stepsize
-        v = Operator.soft_thresholding(v, param.theta * param.stepsize)
+        v = x - grad .* stepsize
+        v = Operator.soft_thresholding(v, theta * stepsize)
         return v
     end
 
-    run_ISTA(alg, obj, grad, param) = run(alg, obj, grad, update_ISTA, param)
+    run_ISTA(alg, obj, grad, stepsize, theta) = run(alg, obj, grad, update_ISTA, stepsize, theta)
+
+    # hoge は適切な名称に変えてください．
+    run_hoge(alg, obj, grad, stepsize, theta) = run(alg, obj, grad, update_hoge, stepsize, theta)
 
     stop_criteria(x, y, tol) = norm(x - y, 2) < tol
 end
--- src/LeastSquares.jl
+++ src/LeastSquares.jl
@@ -31,19 +31,38 @@ rng = MersenneTwister(5)
 x0 = randn(rng, Float64, n)
 maximum_iter = 10
 tol = 1e-1
-alg = Algorithm.IterativeAlgorithm(x0, maximum_iter, tol)
-param = Algorithm.Param(lambda, theta)
 
+result_dir = "./results/least-squares_$n-$m-$sparsity-$theta"
 if !ispath("./results")
     mkdir("./results")
 end
-result_dir = "./results/least-squares_$n-$m-$sparsity-$theta-$lambda"
-@time Algorithm.run_ISTA(alg, obj, grad, param)
-CSV.write(result_dir * ".csv", alg.df)
 
-df = alg.df
+pg = Algorithm.IterativeAlgorithm(x0, maximum_iter, tol)
+@time Algorithm.run_ISTA(pg, obj, grad, lambda, theta)
+CSV.write(result_dir * "-pg.csv", pg.df)
+
+# アルゴリズムを加える場合は Algorithm.jl に run_hoge を書き直しましょう．"hoge" や "HOGE" は適宜変更してください．
+hoge = Algorithm.IterativeAlgorithm(x0, maximum_iter, tol)
+@time Algorithm.run_hoge(hoge, obj, grad, lambda, theta)
+CSV.write(result_dir * "-hoge.csv", hoge.df)
+
+df_pg = pg.df
+df_hoge = hoge.df
 obj_opt = obj(x)
-plot(df.iter, abs.(df.obj .- obj_opt), xaxis="k", yaxis=raw"$\log_{10}|Ψ(x^k) - Ψ(x^*)|$", label="PG", yscale=:log10)
+plot(df_pg.iter, abs.(df_pg.obj .- obj_opt), xaxis="k", yaxis=raw"$\log_{10}|Ψ(x^k) - Ψ(x^*)|$", label="PG", yscale=:log10)
+plot!(df_hoge.iter, abs.(df_hoge.obj .- obj_opt), xaxis="k", yaxis=raw"$\log_{10}|Ψ(x^k) - Ψ(x^*)|$", label="HOGE", yscale=:log10)
 savefig(result_dir * "-diffobj.png")
-plot(df.iter, df.obj, xaxis="k", yaxis=raw"$\log_{10}|Ψ(x^k)|$", label="PG", yscale=:log10)
+print("saved ", result_dir * "-diffobj.png\n")
+plot(df_pg.iter, df_pg.obj, xaxis="k", yaxis=raw"$\log_{10}|Ψ(x^k)|$", label="PG", yscale=:log10)
+plot!(df_hoge.iter, df_hoge.obj, xaxis="k", yaxis=raw"$\log_{10}|Ψ(x^k)|$", label="HOGE", yscale=:log10)
 savefig(result_dir * "-objv.png")
+print("saved ", result_dir * "-objv.png\n")
+
+plot(df_pg.time, abs.(df_pg.obj .- obj_opt), xaxis="time (sec)", yaxis=raw"$\log_{10}|Ψ(x^k) - Ψ(x^*)|$", label="PG", yscale=:log10)
+plot!(df_hoge.time, abs.(df_hoge.obj .- obj_opt), xaxis="time (sec)", yaxis=raw"$\log_{10}|Ψ(x^k) - Ψ(x^*)|$", label="HOGE", yscale=:log10)
+savefig(result_dir * "-diffobj-time.png")
+print("saved ", result_dir * "-diffobj-time.png\n")
+plot(df_pg.time, df_pg.obj, xaxis="time (sec)", yaxis=raw"$\log_{10}|Ψ(x^k)|$", label="PG", yscale=:log10)
+plot!(df_hoge.time, df_hoge.obj, xaxis="time (sec)", yaxis=raw"$\log_{10}|Ψ(x^k)|$", label="HOGE", yscale=:log10)
+savefig(result_dir * "-objv-time.png")
+print("saved ", result_dir * "-objv-time.png\n")
